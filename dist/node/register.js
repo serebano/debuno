5f0d173155b48cd6eea8ddac4ade6e9843da5ab5
serebano <mr.serebano@gmail.com>
2024-12-31 04:09:26 +0200

    chore: bump version to 0.1.30 and update installation script output

--- dist/node/register.js
+++ dist/node/register.js
@@ -1,3 +1,3 @@
-// debuno 0.1.29
-import{c as a}from"../chunk-YGPDSVBY.js";import"../chunk-YRQ3LC62.js";export{a as register};
+// debuno 0.1.31
+import{c as a}from"../chunk-PTWICFHM.js";import"../chunk-SWRUKNDJ.js";export{a as register};
 //# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFtdLAogICJzb3VyY2VzQ29udGVudCI6IFtdLAogICJtYXBwaW5ncyI6ICIiLAogICJuYW1lcyI6IFtdCn0K
--- scripts/init.sh
+++ scripts/init.sh
@@ -6,6 +6,3 @@ export BUN_CREATE_DIR=$_BASE/.bun-create
 _PROJECT_NAME="${1:-debuno}"
 
 bun create debuno $_PROJECT_NAME
-cd $_PROJECT_NAME
-# debuno link
-bun run dev
